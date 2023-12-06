bbfeb7ad8c4df05da50401a57ef4b76f1c7667be
Sid0702 <siddharth.nautiyal@msds.christ.in>
2023-12-06 21:52:49 +0530

    upload other files

---
+++ java.js
@@ -0,0 +1,10 @@
+setTimeout(function(){
+    var typed = new Typed('.element', {
+        strings: ["Siddharth Nautiyal" ,"Data Science Student"],
+        typeSpeed: 40,
+        backSpeed: 10,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 100
+    });
+    },100)
